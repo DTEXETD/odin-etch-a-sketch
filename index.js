2a619e5a3b2b036dd47731b4a558e0d1dfb8375c
dtexetd <dtex.null@gmail.com>
2024-07-14 01:43:21 +0100

    Added select and drag while mouse is down instead of hovering

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 const container = document.querySelector(".container")
-let size = 40
+const colorPicker = document.querySelector("input")
+let size = 2
+let mouseDown = false
 
 const setSize = document.querySelector(':root')
 setSize.style.setProperty('--size', size)
 
+
+
+// Paint only when the mouse is down
+function checkMouseDown(square, color) {
+    square.addEventListener("mousedown", () => {
+        mouseDown = true
+        square.style.backgroundColor = color
+    })
+    
+    square.addEventListener("mouseover", () => {
+        if (mouseDown) {
+            square.style.backgroundColor = color
+        }
+    })
+    document.addEventListener("mouseup", () => {
+        mouseDown = false
+    })
+        
+}
+
+function clearSquares(square) {
+
+}
+
 for(i = 0; i < size * size; i++)  {
     const square = document.createElement('div')
     square.className = "square"
-    container.appendChild(square)
+    
+    checkMouseDown(square, "black")
+        
+        colorPicker.addEventListener("change", (e) => {
+            checkMouseDown(square, e.target.value)
+        })
+
+        container.appendChild(square)
 }
+
+const squares = document.querySelectorAll(".square")
